6ec798e8c008219fbd399a68d4a27348ab160d5b
Xu Chunyang <xuchunyang56@gmail.com>
2021-07-27 16:19:57 +0800

    * src/App.vue: Update.

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,7 @@
 <template>
   <div id="app">
     <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App!!!"/>
+    <HelloWorld msg="WELCOME TO YOUR VUE.JS APP!!!"/>
   </div>
 </template>
 
